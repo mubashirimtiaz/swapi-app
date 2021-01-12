0af2cf2f4e968a389fbc181a91e91a372951c501
mubashirimtiaz <mubashir.imtiaz123@gmail.com>
2021-01-12 23:04:28 +0500

    added full fludge  pagination in people tab

--- src/components/people/People.component.jsx
+++ src/components/people/People.component.jsx
@@ -17,11 +17,28 @@ const People = () => {
       {status === "loading" && <p>loading...</p>}
       {status === "error" && <p>error in fetching data</p>}
       {status === "success" && (
-        <div>
-          {data.results.map((person) => (
-            <Person key={person.name} {...person} />
-          ))}
-        </div>
+        <>
+          <button
+            onClick={() => setPage((old) => Math.max(old - 1, 1))}
+            disabled={page === 1}
+          >
+            Previous
+          </button>
+          <span>{page}</span>
+          <button
+            onClick={() =>
+              setPage((old) => (!data || !data.next ? old : old + 1))
+            }
+            disabled={!data.next}
+          >
+            Next
+          </button>
+          <div>
+            {data.results.map((person) => (
+              <Person key={person.name} {...person} />
+            ))}
+          </div>
+        </>
       )}
     </div>
   );
