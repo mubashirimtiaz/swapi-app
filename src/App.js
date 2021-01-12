23919d470aa9002542d0e57abfc0bce207d267c8
mubashirimtiaz <mubashir.imtiaz123@gmail.com>
2021-01-12 23:19:47 +0500

    remove useEffect to store tab in local storage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Navbar, People, Planets } from "./components";
 import { ReactQueryDevtools } from "react-query/devtools";
 
 const App = () => {
-  const [page, setPage] = useState("planets");
+  const [tab, setTab] = useState(
+    localStorage.getItem("tab") ? localStorage.getItem("tab") : "planets"
+  );
+  useEffect(() => {
+    localStorage.setItem("tab", tab);
+  }, [tab]);
   return (
     <>
       <div className="App">
         <h1>Star Wars Info</h1>
-        <Navbar togglePage={setPage} />
+        <Navbar togglePage={setTab} />
         <div className="content">
-          {page === "planets" ? <Planets /> : <People />}
+          {tab === "planets" ? <Planets /> : <People />}
         </div>
       </div>
       <ReactQueryDevtools initialIsOpen={false} />
